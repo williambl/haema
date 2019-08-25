fd2bfd9add727c708024e259473b667c7f49fb00
williambl <william@williambl.com>
2019-08-25 01:20:30 +0100

    Add debuffs when vampire is in sunlight

---
+++ src/main/kotlin/com/williambl/haema/common/VampireEventHandler.kt
@@ -0,0 +1,31 @@
+package com.williambl.haema.common
+
+import com.williambl.haema.common.capability.VampirismProvider
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.potion.Potion
+import net.minecraft.potion.PotionEffect
+import net.minecraft.util.math.BlockPos
+import net.minecraftforge.event.entity.living.LivingEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+@Mod.EventBusSubscriber
+object VampireEventHandler {
+
+    @SubscribeEvent
+    @JvmStatic
+    fun vampireLivingEvent(e: LivingEvent.LivingUpdateEvent) {
+        if (e.entity.world.isRemote || e.entity !is EntityPlayer || !(e.entity.hasCapability(VampirismProvider.vampirism!!, null)))
+            return
+
+        val entity = e.entity as EntityPlayer
+        val world = entity.world
+
+        if (world.rand.nextDouble() > 0.8 && entity.getActivePotionEffect(Potion.getPotionFromResourceLocation("minecraft:hunger")!!) == null) {
+            if (world.isDaytime && world.canSeeSky(BlockPos(entity.posX, entity.posY + entity.eyeHeight, entity.posZ))) {
+                entity.addPotionEffect(PotionEffect(Potion.getPotionFromResourceLocation("minecraft:hunger")!!, 200, 1))
+            }
+        }
+    }
+
+}
