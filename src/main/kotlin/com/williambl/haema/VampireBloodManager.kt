2897a9d3da2d2ccd1e81bf8b6e3bde3c60d5ef04
williambl <william@williambl.com>
2020-06-12 16:16:07 +0100

    Add extended reach with REA

--- src/main/kotlin/com/williambl/haema/VampireBloodManager.kt
+++ src/main/kotlin/com/williambl/haema/VampireBloodManager.kt
@@ -1,12 +1,22 @@
 package com.williambl.haema
 
+import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes
+import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.player.HungerManager
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.world.GameRules
+import java.util.*
 
 class VampireBloodManager : HungerManager() {
+
+    companion object {
+        private val VAMPIRE_REACH_UUID = UUID.fromString("0eb4fc5f-71d5-4440-b517-bcc18e1df6f4")
+        private val VAMPIRE_ATTACK_RANGE_UUID = UUID.fromString("3267a46b-2b48-429f-a3a8-439aa87a876d")
+        private val VAMPIRE_REACH = EntityAttributeModifier(VAMPIRE_REACH_UUID, "Vampire reach extension", 2.0, EntityAttributeModifier.Operation.ADDITION)
+        private val VAMPIRE_ATTACK_RANGE = EntityAttributeModifier(VAMPIRE_ATTACK_RANGE_UUID, "Vampire attack range extension", 2.0, EntityAttributeModifier.Operation.ADDITION)
+    }
 
     //TODO: make this have effect
     var canSprint = false
@@ -21,8 +31,15 @@ class VampireBloodManager : HungerManager() {
             player.addStatusEffect(StatusEffectInstance(StatusEffects.WEAKNESS, 1, 4-foodLevel))
         }
 
-        if (foodLevel >= 6) {
-            //TODO: extended reach
+        val reachAttr = player.getAttributeInstance(ReachEntityAttributes.REACH)
+        val attackRangeAttr = player.getAttributeInstance(ReachEntityAttributes.ATTACK_RANGE)
+
+        if (foodLevel >= 6 && (reachAttr?.hasModifier(VAMPIRE_REACH) == false || attackRangeAttr?.hasModifier(VAMPIRE_ATTACK_RANGE) == false)) {
+            reachAttr?.addTemporaryModifier(VAMPIRE_REACH)
+            attackRangeAttr?.addTemporaryModifier(VAMPIRE_ATTACK_RANGE)
+        } else if (reachAttr?.hasModifier(VAMPIRE_REACH) != false || attackRangeAttr?.hasModifier(VAMPIRE_ATTACK_RANGE) != false) {
+            reachAttr?.removeModifier(VAMPIRE_REACH)
+            attackRangeAttr?.removeModifier(VAMPIRE_ATTACK_RANGE)
         }
 
         if (foodLevel >= 8) {
