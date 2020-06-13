894b1a30c8842169bf1041aa2b79cbaa8a8ab99f
williambl <william@williambl.com>
2020-06-13 21:40:07 +0100

    Fix vampire status not being transferred after respawns

--- src/main/java/com/williambl/haema/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/williambl/haema/mixin/ServerPlayerEntityMixin.java
@@ -1,31 +1,14 @@
 package com.williambl.haema.mixin;
 
 import com.mojang.authlib.GameProfile;
-import com.williambl.haema.SunlightSicknessEffect;
 import com.williambl.haema.Vampirable;
-import com.williambl.haema.VampireBloodManager;
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.crash.CrashException;
-import net.minecraft.util.crash.CrashReport;
-import net.minecraft.util.crash.CrashReportSection;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
@@ -37,7 +20,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @Inject(method = "copyFrom", at = @At("TAIL"))
     void readVampireData(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
-        ((Vampirable)this).setVampire(((Vampirable)this).isVampire());
+        ((Vampirable)this).setVampire(((Vampirable)oldPlayer).isVampire());
     }
 
 }
