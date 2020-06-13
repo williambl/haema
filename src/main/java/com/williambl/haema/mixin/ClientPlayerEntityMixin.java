3239d9959e50444b7a2117603b7a67568a657aa5
williambl <william@williambl.com>
2020-06-13 14:01:49 +0100

    Add shader effect for vampire vision

---
+++ src/main/java/com/williambl/haema/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,38 @@
+package com.williambl.haema.mixin;
+
+import com.mojang.authlib.GameProfile;
+import com.williambl.haema.SunlightSicknessEffect;
+import com.williambl.haema.VampireBloodManager;
+import com.williambl.haema.VampireEntity;
+import com.williambl.haema.client.HaemaClientKt;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.HungerManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity implements VampireEntity {
+
+    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;tick()V"))
+    void useShaders(CallbackInfo ci) {
+        HaemaClientKt.getVAMPIRE_SHADER().setUniformValue("Saturation", 0.8f * (float) ((VampireBloodManager)this.hungerManager).getBloodLevel()/20.0f);
+    }
+}
--- src/main/kotlin/com/williambl/haema/client/HaemaClient.kt
+++ src/main/kotlin/com/williambl/haema/client/HaemaClient.kt
@@ -1,14 +1,22 @@
 package com.williambl.haema.client
 
 import com.williambl.haema.VampireBloodManager
-import com.williambl.haema.VampireEntity
-import com.williambl.haema.bloodLevelPackeId
+import ladysnake.satin.api.event.ShaderEffectRenderCallback
+import ladysnake.satin.api.managed.ManagedShaderEffect
+import ladysnake.satin.api.managed.ShaderEffectManager
+import ladysnake.satin.api.managed.uniform.Uniform1f
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry
-import net.fabricmc.fabric.api.network.PacketConsumer
 import net.minecraft.util.Identifier
+import kotlin.math.sin
+
+
+val VAMPIRE_SHADER: ManagedShaderEffect = ShaderEffectManager.getInstance()
+    .manage(Identifier("haema", "shaders/post/vampirevision.json"))
+
 
 fun init() {
-    ClientSidePacketRegistry.INSTANCE.register(bloodLevelPackeId) { packetContext, packetByteBuf ->
+    ClientSidePacketRegistry.INSTANCE.register(Identifier("haema:bloodlevelsync")) { packetContext, packetByteBuf ->
         (packetContext.player.hungerManager as VampireBloodManager).absoluteBloodLevel = packetByteBuf.readDouble()
     }
+    ShaderEffectRenderCallback.EVENT.register(ShaderEffectRenderCallback { VAMPIRE_SHADER.render(it) })
 }
