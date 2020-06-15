344eb6b0ffb4c57777e8eecfc89447ffaa78d059
williambl <william@williambl.com>
2020-06-15 16:24:46 +0100

    Add vampire blood and vampire blood injector

--- src/main/kotlin/com/williambl/haema/Haema.kt
+++ src/main/kotlin/com/williambl/haema/Haema.kt
@@ -3,24 +3,34 @@ package com.williambl.haema
 import com.williambl.haema.effect.SunlightSicknessEffect
 import com.williambl.haema.effect.VampiricStrengthEffect
 import com.williambl.haema.effect.VampiricWeaknessEffect
+import com.williambl.haema.util.addTradesToProfession
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.fabricmc.fabric.api.event.player.UseEntityCallback
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder
+import net.fabricmc.fabric.api.loot.v1.FabricLootSupplierBuilder
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback.LootTableSetter
 import net.fabricmc.fabric.api.tag.TagRegistry
 import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.attribute.EntityAttributeModifier
-import net.minecraft.entity.attribute.EntityAttributes
-import net.minecraft.entity.effect.StatusEffect
-import net.minecraft.entity.effect.StatusEffectType
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.loot.ConstantLootTableRange
+import net.minecraft.loot.LootManager
+import net.minecraft.loot.entry.ItemEntry
+import net.minecraft.resource.ResourceManager
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.registry.Registry
+import net.minecraft.village.TradeOffer
+import net.minecraft.village.TradeOffers
+import net.minecraft.village.VillagerProfession
 import net.minecraft.world.World
+
 
 val bloodLevelPackeId = Identifier("haema:bloodlevelsync")
 
@@ -29,6 +39,10 @@ val mediumBloodTag = TagRegistry.entityType(Identifier("haema:medium_blood_sourc
 val poorBloodTag = TagRegistry.entityType(Identifier("haema:poor_blood_sources"))
 
 val vampireEffectiveWeaponsTag = TagRegistry.item(Identifier("haema:vampire_weapons"))
+
+val dungeonLootTable = Identifier("minecraft:chests/simple_dungeon")
+val jungleTempleLootTable = Identifier("minecraft:chests/jungle_temple")
+val desertPyramidLootTable = Identifier("minecraft:chests/desert_pyramid")
 
 fun init() {
     UseBlockCallback.EVENT.register(UseBlockCallback { player, world, hand, blockHitResult ->
@@ -45,6 +59,22 @@ fun init() {
         if ((player as Vampirable).isVampire && entity is LivingEntity && player.isSneaking)
             (player.hungerManager as VampireBloodManager).feed(entity, player)
         else ActionResult.PASS
+    })
+
+    LootTableLoadingCallback.EVENT.register(LootTableLoadingCallback { resourceManager: ResourceManager?, lootManager: LootManager?, id: Identifier?, supplier: FabricLootSupplierBuilder, setter: LootTableSetter? ->
+        if (id == dungeonLootTable || id == jungleTempleLootTable || id == desertPyramidLootTable) {
+            val poolBuilder: FabricLootPoolBuilder = FabricLootPoolBuilder.builder()
+                    .rolls(ConstantLootTableRange.create(1))
+                    .withEntry(ItemEntry.builder(Registry.ITEM.get(Identifier("haema:vampire_blood")))
+                            .weight(if (id == dungeonLootTable) 10 else 5)
+                            .build()
+                    )
+                    .withEntry(ItemEntry.builder(Items.AIR)
+                            .weight(10)
+                            .build()
+                    )
+            supplier.withPool(poolBuilder.build())
+        }
     })
 
     Registry.register(
@@ -75,6 +105,32 @@ fun init() {
                     Vampirable.convert(user)
                     return TypedActionResult.consume(user.getStackInHand(hand))
                 }
+            }
+    )
+
+    Registry.register(
+            Registry.ITEM,
+            Identifier("haema:vampire_blood"),
+            Item(Item.Settings().group(ItemGroup.MISC))
+    )
+
+    Registry.register(
+            Registry.ITEM,
+            Identifier("haema:vampire_blood_injector"),
+            Item(Item.Settings().group(ItemGroup.TOOLS))
+    )
+
+    addTradesToProfession(
+            VillagerProfession.CLERIC,
+            3,
+            TradeOffers.Factory { _, _ ->
+                TradeOffer(
+                        ItemStack(Items.EMERALD, 5),
+                        ItemStack(Registry.ITEM.get(Identifier("haema:vampire_blood"))),
+                        1,
+                        30,
+                        0.05f
+                )
             }
     )
 }
