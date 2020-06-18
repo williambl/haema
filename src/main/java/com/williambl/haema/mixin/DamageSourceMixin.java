b4966053c0afbcf1c29a9a4396b53b5d278cd1f2
williambl <william@williambl.com>
2020-06-18 16:35:49 +0100

    Make Smite effective against vampires

--- src/main/java/com/williambl/haema/mixin/DamageSourceMixin.java
+++ src/main/java/com/williambl/haema/mixin/DamageSourceMixin.java
@@ -2,9 +2,12 @@ package com.williambl.haema.mixin;
 
 import com.williambl.haema.HaemaKt;
 import com.williambl.haema.damagesource.DamageSourceExtensionsKt;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.EntityDamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +19,8 @@ public class DamageSourceMixin {
     @Inject(method = "player", at = @At("HEAD"), cancellable = true)
     private static void addVampireEffectiveSources(PlayerEntity attacker, CallbackInfoReturnable<DamageSource> cir) {
         DamageSource source = new EntityDamageSource("player", attacker);
-        if (attacker.getMainHandStack().getItem().isIn(HaemaKt.getVampireEffectiveWeaponsTag()))
+        ItemStack stack = attacker.getMainHandStack();
+        if (stack.getItem().isIn(HaemaKt.getVampireEffectiveWeaponsTag()) || EnchantmentHelper.getLevel(Enchantments.SMITE, stack) >= 1)
             DamageSourceExtensionsKt.setEffectiveAgainstVampires(source);
         cir.setReturnValue(source);
     }
