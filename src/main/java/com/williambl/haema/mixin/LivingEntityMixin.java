73f78ab9ca2054408019a24adc59b4845896dd5d
williambl <william@williambl.com>
2020-07-01 11:42:23 +0100

    Fix vampires not having death messages

---
+++ src/main/java/com/williambl/haema/mixin/LivingEntityMixin.java
@@ -0,0 +1,40 @@
+package com.williambl.haema.mixin;
+
+import com.williambl.haema.Vampirable;
+import com.williambl.haema.damagesource.DamageSourceExtensionsKt;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+
+    @Shadow public abstract float getHealth();
+
+    @Shadow public abstract boolean isDead();
+
+    @Unique private DamageSource currentSource;
+
+    @Inject(method = "damage", at = @At(value = "HEAD"))
+    void setCurrentSource(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        currentSource = source;
+    }
+
+    @Redirect(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isDead()Z", ordinal = 1))
+    boolean isActuallyDead(LivingEntity livingEntity) {
+        if (livingEntity instanceof PlayerEntity && ((Vampirable)livingEntity).isVampire()) {
+            DamageSource theCurrentSource = currentSource;
+            currentSource = null;
+            return this.getHealth() <= 0 && DamageSourceExtensionsKt.isEffectiveAgainstVampires(theCurrentSource);
+        }
+        return this.isDead();
+    }
+}
