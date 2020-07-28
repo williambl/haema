a5b352ae6cd015cbed424a68a8ce4e9c3ad2472a
williambl <william@williambl.com>
2020-07-28 22:41:53 +0100

    Make origin-vampires spawn with the Book of Blood

--- src/main/kotlin/com/williambl/haema/plugin/origins/VampirePower.kt
+++ src/main/kotlin/com/williambl/haema/plugin/origins/VampirePower.kt
@@ -9,10 +9,14 @@ import io.github.apace100.origins.power.PowerType
 import net.minecraft.block.BedBlock
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.Identifier
 import net.minecraft.util.Pair
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.registry.Registry
 
 class VampirePower(type: PowerType<*>?, player: PlayerEntity?) : Power(type, player) {
     override fun onAdded() {
@@ -27,6 +31,13 @@ class VampirePower(type: PowerType<*>?, player: PlayerEntity?) : Power(type, pla
 
     override fun onChosen(isOrbOfOrigin: Boolean) {
         (player.hungerManager as VampireBloodManager).addBlood(8.0)
+
+        val bookStack = ItemStack(Registry.ITEM[Identifier("patchouli:guide_book")])
+        val tag = CompoundTag()
+        tag.putString("patchouli:book", "haema:book_of_blood")
+        bookStack.tag = tag
+        player.inventory.offerOrDrop(player.world, bookStack)
+
         if (isOrbOfOrigin) return
         if (player is ServerPlayerEntity) {
             val serverPlayer = player as ServerPlayerEntity
