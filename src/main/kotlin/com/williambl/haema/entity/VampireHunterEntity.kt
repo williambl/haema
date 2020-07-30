9b10c6f725fff5c70a90e8f6ce81d464709b3598
williambl <william@williambl.com>
2020-07-30 22:30:49 +0100

    Add custom ai goals for vampire hunter crossbow and sword attacking

--- src/main/kotlin/com/williambl/haema/entity/VampireHunterEntity.kt
+++ src/main/kotlin/com/williambl/haema/entity/VampireHunterEntity.kt
@@ -20,12 +20,13 @@ import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.ListTag
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
+import net.minecraft.util.Hand
 import net.minecraft.world.LocalDifficulty
 import net.minecraft.world.World
 import net.minecraft.world.WorldAccess
 
 class VampireHunterEntity(entityType: EntityType<out RaiderEntity>?, world: World?) : RaiderEntity(entityType, world), CrossbowUser {
-    private val inventory = SimpleInventory(5)
+    val inventory = SimpleInventory(5)
 
     override fun initialize(world: WorldAccess?, difficulty: LocalDifficulty?, spawnReason: SpawnReason?, entityData: EntityData?, entityTag: CompoundTag?): EntityData? {
         initEquipment(difficulty)
@@ -35,7 +36,8 @@ class VampireHunterEntity(entityType: EntityType<out RaiderEntity>?, world: Worl
     override fun initGoals() {
         super.initGoals()
         goalSelector.add(0, SwimGoal(this))
-        goalSelector.add(3, CrossbowAttackGoal(this, 1.0, 8.0f))
+        goalSelector.add(2, VampireHunterCrossbowAttackGoal(this, 1.0, 8.0f))
+        goalSelector.add(3, VampireHunterMeleeAttackGoal(this, 1.0, true))
         goalSelector.add(8, WanderAroundGoal(this, 0.6))
         goalSelector.add(9, LookAtEntityGoal(this, PlayerEntity::class.java, 15.0f, 1.0f))
         goalSelector.add(10, LookAtEntityGoal(this, MobEntity::class.java, 15.0f))
@@ -129,3 +131,70 @@ class VampireHunterEntity(entityType: EntityType<out RaiderEntity>?, world: Worl
     }
 }
 
+class VampireHunterCrossbowAttackGoal(private val actor: VampireHunterEntity, speed: Double, range: Float) : CrossbowAttackGoal<VampireHunterEntity>(actor, speed, range) {
+    override fun canStart(): Boolean = hasValidTarget() && actorHasCrossbow()
+
+    override fun shouldContinue(): Boolean =
+        hasValidTarget() && (canStart() || !this.actor.navigation.isIdle) && actorHasCrossbow()
+
+    private fun hasValidTarget(): Boolean = actor.target != null && actor.target!!.isAlive && actor.squaredDistanceTo(actor.target!!) > 16 && actor.target!!.health > 4
+
+    private fun actorHasCrossbow(): Boolean =
+        actor.isHolding(Items.CROSSBOW) || actor.inventory.containsAny(setOf(Items.CROSSBOW))
+
+    override fun start() {
+        super.start()
+        if (!actor.isHolding(Items.CROSSBOW)) {
+            val equipped = actor.getStackInHand(Hand.MAIN_HAND)
+            actor.equipStack(EquipmentSlot.MAINHAND, actor.inventory.removeItem(Items.CROSSBOW, 1))
+            actor.inventory.addStack(equipped)
+        }
+    }
+
+    override fun stop() {
+        super.stop()
+        if (actor.isHolding(Items.CROSSBOW)) {
+            val equipped = actor.getStackInHand(Hand.MAIN_HAND)
+            if (actor.inventory.canInsert(equipped)) {
+                actor.equipStack(EquipmentSlot.MAINHAND, ItemStack.EMPTY)
+                actor.inventory.addStack(equipped)
+            }
+        }
+    }
+}
+
+class VampireHunterMeleeAttackGoal(private val actor: VampireHunterEntity, speed: Double, pauseWhenMobIdle: Boolean) : MeleeAttackGoal(actor, speed, pauseWhenMobIdle) {
+    override fun canStart(): Boolean = super.canStart() && hasValidTarget() && actorHasSword()
+
+    override fun shouldContinue(): Boolean = super.shouldContinue() && hasValidTarget() && actorHasSword()
+
+    private fun hasValidTarget(): Boolean = actor.target != null && actor.target!!.isAlive
+
+    private fun actorHasSword(): Boolean =
+        actor.isHolding(Items.WOODEN_SWORD) || actor.inventory.containsAny(setOf(Items.WOODEN_SWORD))
+
+    override fun start() {
+        super.start()
+        if (!actor.isHolding(Items.WOODEN_SWORD)) {
+            val equipped = actor.getStackInHand(Hand.MAIN_HAND)
+            actor.equipStack(EquipmentSlot.MAINHAND, actor.inventory.removeItem(Items.WOODEN_SWORD, 1))
+            actor.inventory.addStack(equipped)
+        }
+    }
+
+    override fun tick() {
+        if (actor.target == null) return
+        super.tick()
+    }
+
+    override fun stop() {
+        super.stop()
+        if (actor.isHolding(Items.WOODEN_SWORD)) {
+            val equipped = actor.getStackInHand(Hand.MAIN_HAND)
+            if (actor.inventory.canInsert(equipped)) {
+                actor.equipStack(EquipmentSlot.MAINHAND, ItemStack.EMPTY)
+                actor.inventory.addStack(equipped)
+            }
+        }
+    }
+}
