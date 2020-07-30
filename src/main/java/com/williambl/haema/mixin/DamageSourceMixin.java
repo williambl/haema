5d1cfe1c021b111180dcac96a5f217cfd4b92faf
williambl <william@williambl.com>
2020-07-30 21:07:27 +0100

    Remove early return for DamageSourceMixin and add support for mobs carrying vampire-effective weapons

--- src/main/java/com/williambl/haema/mixin/DamageSourceMixin.java
+++ src/main/java/com/williambl/haema/mixin/DamageSourceMixin.java
@@ -4,8 +4,8 @@ import com.williambl.haema.HaemaKt;
 import com.williambl.haema.damagesource.DamageSourceExtensionsKt;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.EntityDamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,12 +16,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(DamageSource.class)
 public class DamageSourceMixin {
 
-    @Inject(method = "player", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "player", at = @At("RETURN"))
     private static void addVampireEffectiveSources(PlayerEntity attacker, CallbackInfoReturnable<DamageSource> cir) {
-        DamageSource source = new EntityDamageSource("player", attacker);
+        DamageSource source = cir.getReturnValue();
         ItemStack stack = attacker.getMainHandStack();
         if (stack.getItem().isIn(HaemaKt.getVampireEffectiveWeaponsTag()) || EnchantmentHelper.getLevel(Enchantments.SMITE, stack) >= 1)
             DamageSourceExtensionsKt.setEffectiveAgainstVampires(source);
-        cir.setReturnValue(source);
+    }
+
+    @Inject(method = "mob", at = @At("TAIL"))
+    private static void addVampireEffectiveSourcesMob(LivingEntity attacker, CallbackInfoReturnable<DamageSource> cir) {
+        DamageSource source = cir.getReturnValue();
+        ItemStack stack = attacker.getMainHandStack();
+        if (stack.getItem().isIn(HaemaKt.getVampireEffectiveWeaponsTag()) || EnchantmentHelper.getLevel(Enchantments.SMITE, stack) >= 1)
+            DamageSourceExtensionsKt.setEffectiveAgainstVampires(source);
     }
 }
