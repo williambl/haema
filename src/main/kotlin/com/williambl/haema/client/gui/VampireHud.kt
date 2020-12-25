ebad8e6d2d31912c0aacf3b4832643f117c693ae
williambl <william@williambl.com>
2020-12-25 02:10:27 +0000

    Add text about feeding in HUD

--- src/main/kotlin/com/williambl/haema/client/gui/VampireHud.kt
+++ src/main/kotlin/com/williambl/haema/client/gui/VampireHud.kt
@@ -1,37 +1,72 @@
 package com.williambl.haema.client.gui
 
-import com.mojang.blaze3d.systems.RenderSystem
-import com.williambl.haema.Vampirable
-import com.williambl.haema.VampireBloodManager
+import com.williambl.haema.*
 import com.williambl.haema.client.ClientVampire
 import com.williambl.haema.client.DASH_KEY
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawableHelper
-import net.minecraft.client.options.KeyBinding
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.text.LiteralText
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
 import net.minecraft.text.TranslatableText
 import net.minecraft.util.Formatting
+import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.util.hit.HitResult
 
 object VampireHud : DrawableHelper() {
 
     fun render(matrixStack: MatrixStack, tickDelta: Float) {
         matrixStack.push()
-        val width = MinecraftClient.getInstance().window.scaledWidth
-        val height = MinecraftClient.getInstance().window.scaledHeight
-        val textRenderer = MinecraftClient.getInstance().textRenderer
-        val player = MinecraftClient.getInstance().player
+
+        val mc = MinecraftClient.getInstance()
+        val width = mc.window.scaledWidth
+        val height = mc.window.scaledHeight
+        val textRenderer = mc.textRenderer
+        val player = mc.player
+
+        if (!(player as Vampirable).isVampire)
+            return
+
         (player as Vampirable).checkBloodManager()
 
         val texts = mutableListOf<Text>()
 
-        val hasEnoughBlood = (player as Vampirable).isVampire && (player.hungerManager as VampireBloodManager).getBloodLevel() > 18f
-        val canDash = (player as Vampirable).isVampire && (player as ClientVampire).canDash()
+        if ((player.hungerManager as VampireBloodManager).getBloodLevel() > 18f) {
+            texts.add(createText(
+                DASH_KEY.boundKeyLocalizedText.copy(),
+                (player as Vampirable).isVampire && (player as ClientVampire).canDash(),
+                TranslatableText("gui.haema.hud.vampiredash"))
+            )
+        }
 
-        if (hasEnoughBlood) {
-            texts.add(createText(DASH_KEY.boundKeyLocalizedText.copy(), if (canDash) Formatting.GREEN else Formatting.RED, TranslatableText("gui.haema.hud.vampiredash")))
+        if (mc.crosshairTarget != null && mc.crosshairTarget!!.type == HitResult.Type.ENTITY) {
+            val lookingAt = (mc.crosshairTarget as EntityHitResult).entity.type
+            if (poorBloodTag.contains(lookingAt) || mediumBloodTag.contains(lookingAt) || goodBloodTag.contains(lookingAt)) {
+                if (player.isSneaking) {
+                    texts.add(
+                        TranslatableText("gui.haema.hud.bloodquality").append(
+                            when {
+                                goodBloodTag.contains(lookingAt) -> TranslatableText("gui.haema.hud.bloodquality.good").formatted(
+                                    Formatting.GREEN
+                                )
+                                mediumBloodTag.contains(lookingAt) -> TranslatableText("gui.haema.hud.bloodquality.medium").formatted(
+                                    Formatting.YELLOW
+                                )
+                                else -> TranslatableText("gui.haema.hud.bloodquality.poor").formatted(Formatting.RED)
+                            }
+                        )
+                    )
+                }
+
+                texts.add(
+                    createText(
+                        TranslatableText("key.sneak").append(" + ").append(mc.options.keyUse.boundKeyLocalizedText),
+                        true,
+                        TranslatableText("gui.haema.hud.feed")
+                    )
+                )
+            }
         }
 
         texts.forEachIndexed { index, text ->
@@ -40,7 +75,7 @@ object VampireHud : DrawableHelper() {
                     MinecraftClient.getInstance().textRenderer,
                     text,
                     width - textRenderer.getWidth(text) / 2 - 10,
-                    height - 16 - index*9,
+                    height - 16 - (index+1)*9,
                     0xffffff
             )
         }
@@ -48,12 +83,8 @@ object VampireHud : DrawableHelper() {
     }
 
 
-    private fun createText(key: MutableText, formatting: Formatting, description: Text): Text {
-        val keyText = key.formatted(formatting)
-        val text = LiteralText("[")
-        text.siblings.add(keyText)
-        text.siblings.add(LiteralText("] "))
-        text.siblings.add(description)
-        return text
+    private fun createText(key: MutableText, available: Boolean, description: Text): Text {
+        val keyText = key.formatted(if (available) Formatting.GREEN else Formatting.RED)
+        return LiteralText("[").append(keyText).append("] ").append(description)
     }
 }
