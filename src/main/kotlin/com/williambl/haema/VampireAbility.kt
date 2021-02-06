7945fc77e75eb2d5c33c1381a5e6b0c37ac7358b
williambl <william@williambl.com>
2021-02-06 18:38:45 +0000

    Add max levels for abilities

--- src/main/kotlin/com/williambl/haema/VampireAbility.kt
+++ src/main/kotlin/com/williambl/haema/VampireAbility.kt
@@ -1,10 +1,10 @@
 package com.williambl.haema
 
-enum class VampireAbility {
+enum class VampireAbility(val maxLevel: Int = Int.MAX_VALUE) {
     NONE,
-    STRENGTH,
-    DASH,
-    INVISIBILITY,
-    IMMORTALITY,
-    VISION
+    STRENGTH(3),
+    DASH(1),
+    INVISIBILITY(1),
+    IMMORTALITY(1),
+    VISION(1)
 }
