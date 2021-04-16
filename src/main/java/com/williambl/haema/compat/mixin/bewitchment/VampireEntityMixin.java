942867b410388b1feff3aa9890f6d89666835604
williambl <william@williambl.com>
2021-04-16 23:10:39 +0100

    Make bewitchment vampire mobs hostile to vampire hunters

--- src/main/java/com/williambl/haema/compat/mixin/bewitchment/VampireEntityMixin.java
+++ src/main/java/com/williambl/haema/compat/mixin/bewitchment/VampireEntityMixin.java
@@ -2,19 +2,31 @@ package com.williambl.haema.compat.mixin.bewitchment;
 
 import com.williambl.haema.Vampirable;
 import com.williambl.haema.abilities.VampireAbility;
+import com.williambl.haema.hunter.VampireHunterEntity;
 import moriyashiine.bewitchment.common.entity.living.VampireEntity;
 import moriyashiine.bewitchment.common.entity.living.util.BWHostileEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.FollowTargetGoal;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VampireEntity.class)
 public abstract class VampireEntityMixin extends BWHostileEntity implements Vampirable {
     protected VampireEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    void haema$initGoals(CallbackInfo ci) {
+        targetSelector.add(1, new FollowTargetGoal<>(this, LivingEntity.class, 10, true, false,
+                (entity) -> entity instanceof VampireHunterEntity));
     }
 
     @Override
