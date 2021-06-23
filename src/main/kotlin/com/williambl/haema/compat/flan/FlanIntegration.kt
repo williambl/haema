1ff347542bfd0cb96f94c9a7846e4ee4cba0dfb6
williambl <william@williambl.com>
2021-06-23 01:30:43 +0100

    Update dependencies

--- src/main/kotlin/com/williambl/haema/compat/flan/FlanIntegration.kt
+++ src/main/kotlin/com/williambl/haema/compat/flan/FlanIntegration.kt
@@ -1,7 +1,7 @@
 package com.williambl.haema.compat.flan
 
-import com.flemmli97.flan.event.EntityInteractEvents
 import com.williambl.haema.api.BloodDrinkingEvents
+import io.github.flemmli97.flan.event.EntityInteractEvents
 
 fun initFlanIntegration() {
         BloodDrinkingEvents.CANCEL.register(BloodDrinkingEvents.CancelBloodDrinkEvent { player, world, hand, target, entityHitResult ->
