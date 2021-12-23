820671be5d38e436b6f93716103b095c96369283
williambl <william@williambl.com>
2021-12-23 00:21:26 +0000

    Remove flan integration for now

--- src/main/kotlin/com/williambl/haema/compat/flan/FlanIntegration.kt
+++ src/main/kotlin/com/williambl/haema/compat/flan/FlanIntegration.kt
@@ -1,14 +1,11 @@
 package com.williambl.haema.compat.flan
 
-import com.williambl.haema.api.BloodDrinkingEvents
-import io.github.flemmli97.flan.event.EntityInteractEvents
-
 fun initFlanIntegration() {
-        BloodDrinkingEvents.CANCEL.register(BloodDrinkingEvents.CancelBloodDrinkEvent { player, world, hand, target, entityHitResult ->
+        /*BloodDrinkingEvents.CANCEL.register(BloodDrinkingEvents.CancelBloodDrinkEvent { player, world, hand, target, entityHitResult ->
             if (entityHitResult != null) {
                 !EntityInteractEvents.attackSimple(player, entityHitResult.entity, true).isAccepted
             } else {
                 true
             }
-        })
+        })*/
 }
