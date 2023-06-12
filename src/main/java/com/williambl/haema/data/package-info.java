9d935b812923de5b623a0232a2fcac8a49335376
williambl <william@williambl.com>
2023-06-12 12:31:12 +0100

    chore: add package-info.java files

---
+++ src/main/java/com/williambl/haema/data/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package com.williambl.haema.data;
+
+import org.jetbrains.annotations.ApiStatus;
---
+++ src/main/java/com/williambl/haema/vampire/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package com.williambl.haema.vampire;
+
+import org.jetbrains.annotations.ApiStatus;
