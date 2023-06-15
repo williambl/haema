b00a9969ec7fe9889e5cb4ec89cdfa506589bdbe
williambl <william@williambl.com>
2023-06-15 20:50:20 +0100

    feat(wip): ritual recipe deserialisation

--- src/main/java/com/williambl/haema/ritual/ritual/RitualAction.java
+++ src/main/java/com/williambl/haema/ritual/ritual/RitualAction.java
@@ -1,8 +1,25 @@
 package com.williambl.haema.ritual.ritual;
 
+import com.mojang.serialization.Codec;
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.KeyDispatchDataCodec;
 
+import java.util.function.Function;
+
+import static com.williambl.haema.Haema.id;
+
 public interface RitualAction {
+    ResourceKey<Registry<Codec<? extends RitualAction>>> RESOURCE_KEY
+            = ResourceKey.createRegistryKey(id("ritual_action"));
+    Registry<Codec<? extends RitualAction>> REGISTRY
+            = FabricRegistryBuilder.createSimple(RESOURCE_KEY).buildAndRegister();
+
+    Codec<RitualAction> ACTION_CODEC = REGISTRY.byNameCodec().dispatch(
+            p -> p.codec().codec(),
+            Function.identity()
+    );
 
     KeyDispatchDataCodec<? extends RitualAction> codec();
 }
--- src/main/java/com/williambl/haema/ritual/ritual/RitualRecipe.java
+++ src/main/java/com/williambl/haema/ritual/ritual/RitualRecipe.java
@@ -1,9 +1,10 @@
 package com.williambl.haema.ritual.ritual;
 
 import com.google.gson.JsonObject;
-import com.mojang.datafixers.util.Either;
 import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.williambl.dfunc.api.DFunction;
 import com.williambl.dfunc.api.context.DFContext;
 import com.williambl.dfunc.api.context.DFContextSpec;
@@ -11,14 +12,13 @@ import com.williambl.haema.Haema;
 import com.williambl.haema.HaemaUtil;
 import com.williambl.haema.api.ritual.RitualArae;
 import com.williambl.haema.ritual.HaemaRituals;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.RegistryCodecs;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
@@ -26,18 +26,22 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
+import xyz.nucleoid.codecs.MoreCodecs;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public record RitualRecipe(
         ResourceLocation id,
         Fluid fluid,
         List<Ingredient> ingredients,
-        Either<ResourceKey<RitualArae>, TagKey<RitualArae>> acceptableAraes,
+        HolderSet<RitualArae> acceptableAraes,
         DFunction<Boolean> canPlayerUse,
         List<RitualAction> actions) implements Recipe<RitualContainer> {
+    public RitualRecipe(Partial partial, ResourceLocation id) {
+        this(id, partial.fluid(), partial.ingredients(), partial.acceptableAraes(), partial.canPlayerUse(), partial.actions());
+    }
+
     @Override
     public boolean matches(RitualContainer container, Level level) {
         return this.canPlayerUse.apply(DFContext.entity(container.player()))
@@ -47,10 +51,7 @@ public record RitualRecipe(
     }
 
     private boolean isAraeAcceptable(RitualArae toCheck, Registry<RitualArae> registry) {
-        return this.acceptableAraes.map(
-                r -> registry.getResourceKey(toCheck).filter(r::equals).isPresent(),
-                t -> registry.getOrCreateTag(t).contains(registry.wrapAsHolder(toCheck))
-        );
+        return this.acceptableAraes.contains(registry.wrapAsHolder(toCheck));
     }
 
     @Override
@@ -87,28 +88,29 @@ public record RitualRecipe(
         return HaemaRituals.RitualRecipeTypes.RITUAL;
     }
 
+    private record Partial(
+            Fluid fluid,
+            List<Ingredient> ingredients,
+            HolderSet<RitualArae> acceptableAraes,
+            DFunction<Boolean> canPlayerUse,
+            List<RitualAction> actions
+    ) {
+        private static final Codec<Partial> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                BuiltInRegistries.FLUID.byNameCodec().fieldOf("fluid").forGetter(Partial::fluid),
+                MoreCodecs.INGREDIENT.listOf().fieldOf("ingredients").forGetter(Partial::ingredients),
+                RegistryCodecs.homogeneousList(RitualArae.REGISTRY_KEY).fieldOf("acceptable_araes").forGetter(Partial::acceptableAraes),
+                DFunction.PREDICATE.codec().comapFlatMap(HaemaUtil.verifyDFunction(DFContextSpec.ENTITY), Function.identity()).fieldOf("can_player_use").forGetter(Partial::canPlayerUse),
+                RitualAction.ACTION_CODEC.listOf().fieldOf("actions").forGetter(Partial::actions)
+        ).apply(instance, Partial::new));
+    }
+
     public static class Serializer implements RecipeSerializer<RitualRecipe> {
         @Override
         public RitualRecipe fromJson(ResourceLocation id, JsonObject jsonObject) {
-            Fluid fluid = BuiltInRegistries.FLUID.get(new ResourceLocation(GsonHelper.getAsString(jsonObject, "fluid")));
-            List<Ingredient> ingredients = new ArrayList<>();
-            var ingredientsJson = jsonObject.getAsJsonArray("ingredients");
-            for (var ingredientJson : ingredientsJson) {
-                ingredients.add(Ingredient.fromJson(ingredientJson));
-            }
-            Either<ResourceKey<RitualArae>, TagKey<RitualArae>> acceptableAraes;
-            String acceptableAraesString = GsonHelper.getAsString(jsonObject, "acceptable_araes");
-            if (acceptableAraesString.startsWith("#")) {
-                acceptableAraes = Either.right(TagKey.create(RitualArae.REGISTRY_KEY, new ResourceLocation(acceptableAraesString.substring(1))));
-            } else {
-                acceptableAraes = Either.left(ResourceKey.create(RitualArae.REGISTRY_KEY, new ResourceLocation(acceptableAraesString)));
-            }
-            DFunction<Boolean> canPlayerUse = DFunction.PREDICATE.codec().decode(JsonOps.INSTANCE, GsonHelper.getAsJsonObject(jsonObject, "can_player_use"))
+            var partial = Partial.CODEC.decode(JsonOps.INSTANCE, jsonObject)
                     .map(Pair::getFirst)
-                    .flatMap(HaemaUtil.verifyDFunction(DFContextSpec.ENTITY))
-                    .getOrThrow(false, e -> Haema.LOGGER.error("Error decoding DFunction for Ritual: {}", e));
-            //TODO ritual actions
-            return new RitualRecipe(id, fluid, ingredients, acceptableAraes, canPlayerUse, List.of());
+                    .getOrThrow(false, e -> Haema.LOGGER.error("Error deserialising ritual {}: {}", id, e));
+            return new RitualRecipe(partial, id);
         }
 
         @Override
@@ -124,7 +126,6 @@ public record RitualRecipe(
             for (var ingredient : recipe.ingredients()) {
                 ingredient.toNetwork(friendlyByteBuf);
             }
-            friendlyByteBuf.writeCharSequence(recipe.acceptableAraes().map(r -> r.location().toString(), t -> "#"+ t.location()), StandardCharsets.UTF_8);
             //TODO finish
         }
     }
