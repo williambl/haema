db62d42633ae78920afa6c20e9c0b3702723e95a
williambl <william@williambl.com>
2023-07-08 21:51:03 +0100

    feat(wip): add base of vampiragers

---
+++ src/main/java/com/williambl/haema/vampire_mobs/HaemaVampireMobs.java
@@ -0,0 +1,30 @@
+package com.williambl.haema.vampire_mobs;
+
+import com.williambl.haema.api.vampire.VampirismSource;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.fabricmc.fabric.impl.object.builder.FabricEntityType;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
+
+import java.util.Optional;
+
+import static com.williambl.haema.Haema.id;
+
+public class HaemaVampireMobs {
+    public static class VampireMobEntityTypes {
+        public static final EntityType<Vampirager> VAMPIRAGER = Registry.register(BuiltInRegistries.ENTITY_TYPE, id("vampirager"), FabricEntityTypeBuilder.createMob().spawnGroup(MobCategory.MONSTER).entityFactory(Vampirager::new).dimensions(EntityDimensions.fixed(0.6f, 1.95f)).spawnRestriction(SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Monster::checkMonsterSpawnRules).build());
+    }
+
+    public static class VampireMobVampirismSources {
+        public static final ResourceKey<VampirismSource> VAMPIRAGER_SPAWN = ResourceKey.create(VampirismSource.REGISTRY_KEY, id("vampirager_spawn"));
+    }
+
+    public static class VampireMobMemoryModuleTypes {
+        public static final MemoryModuleType<LivingEntity> BLOOD_DRINKING_TARGET = Registry.register(BuiltInRegistries.MEMORY_MODULE_TYPE, id("blood_drinking_target"), new MemoryModuleType<>(Optional.empty()));
+    }
+}
---
+++ src/main/java/com/williambl/haema/vampire_mobs/Vampirager.java
@@ -0,0 +1,111 @@
+package com.williambl.haema.vampire_mobs;
+
+import com.williambl.haema.Haema;
+import com.williambl.haema.api.content.blood.BloodApi;
+import com.williambl.haema.api.vampire.VampireApi;
+import com.williambl.haema.api.vampire.VampireComponent;
+import com.williambl.haema.api.vampire.VampirismSource;
+import com.williambl.haema.hunters.VampireHunter;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableMeleeAttack;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.AvoidSun;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.EscapeSun;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.FloatToSurfaceOfFluid;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetPlayerLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliate;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyPlayersSensor;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Vampirager extends Monster implements SmartBrainOwner<Vampirager> {
+    protected Vampirager(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Nullable
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor serverLevelAccessor, DifficultyInstance difficultyInstance, MobSpawnType mobSpawnType, @Nullable SpawnGroupData spawnGroupData, @Nullable CompoundTag compoundTag) {
+        var res = super.finalizeSpawn(serverLevelAccessor, difficultyInstance, mobSpawnType, spawnGroupData, compoundTag);
+        var converted = VampireComponent.KEY.get(this).tryConvert(serverLevelAccessor.registryAccess().registryOrThrow(VampirismSource.REGISTRY_KEY).get(HaemaVampireMobs.VampireMobVampirismSources.VAMPIRAGER_SPAWN));
+        if (!converted) {
+            Haema.LOGGER.warn("Failed to set vampirager {} as vampire", this);
+        }
+        return res;
+    }
+
+    @Override
+    protected Brain.Provider<?> brainProvider() {
+        return new SmartBrainProvider<>(this);
+    }
+
+    @Override
+    public List<ExtendedSensor<Vampirager>> getSensors() {
+        return ObjectArrayList.of(
+                new NearbyPlayersSensor<>(),
+                new NearbyLivingEntitySensor<Vampirager>()
+                        .setPredicate((target, entity) -> target != entity && (target instanceof VampireHunter || target instanceof AbstractVillager || BloodApi.getBloodQuality(target).isPresent())));
+    }
+
+    @Override
+    public BrainActivityGroup<Vampirager> getCoreTasks() {
+        return BrainActivityGroup.coreTasks(
+            new FloatToSurfaceOfFluid<>(),
+                new AvoidSun<>(),
+                new EscapeSun<>().cooldownFor($ -> 20),
+                new LookAtTarget<>(),
+                new DashToTarget<>(),
+                new MoveToWalkTarget<>());
+    }
+
+    @Override
+    public BrainActivityGroup<Vampirager> getIdleTasks() {
+        return BrainActivityGroup.idleTasks(
+                new FirstApplicableBehaviour<>(
+                        new TargetOrRetaliate<>(),
+                        new InvalidateDrinkTarget<>(),
+                        new DrinkFromDrinkTarget<>(),
+                        new SetWalkTargetToDrinkTarget<>(),
+                        new SetDrinkTarget<>(),
+                        new SetPlayerLookTarget<>(),
+                        new SetRandomLookTarget<>()),
+                new OneRandomBehaviour<>(
+                        new SetRandomWalkTarget<>().speedModifier(1),
+                        new Idle<>().runFor(entity -> entity.getRandom().nextInt(30, 60))));
+    }
+
+    @Override
+    public BrainActivityGroup<Vampirager> getFightTasks() {
+        return BrainActivityGroup.fightTasks(
+                new InvalidateAttackTarget<>(),
+                new SetWalkTargetToAttackTarget<>(),
+                new FirstApplicableBehaviour<>(
+                        new DrinkBloodFromAttackTarget<>(),
+                        new SpawnReinforcements<>(),
+                        new AnimatableMeleeAttack<>(0).whenStarting(entity -> this.setAggressive(true)).whenStarting(entity -> this.setAggressive(false))));
+    }
+}
